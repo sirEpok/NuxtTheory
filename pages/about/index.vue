f6f0b990068797b79037b9463c131c596e44067e
Anton <sir.bazhin@yandex.ru>
2020-11-22 14:57:19 +0500

    TestNuxt

---
+++ pages/about/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h1>About Page</h1>
+
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum officiis voluptatum dolores ullam hic, nobis deserunt distinctio explicabo provident voluptates veniam dolorem nihil fuga, iste illum itaque, tenetur repellat quo.</p>
+
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia eaque perferendis molestias delectus quam. Optio modi architecto reprehenderit? Reiciendis animi perferendis possimus accusamus! Eos hic optio, rerum dolorum accusantium autem.</p>
+  </section>
+</template>
+
+<script>
+export default {
+  middleware: ['auth']
+}
+</script>
---
+++ pages/users/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <section>
+    <h1>{{pageTitle}}</h1>
+
+    <ul>
+      <li v-for="user of users" :key="user.id">
+        <a href="#" @click.prevent="goTo(user)">{{user.name}} ({{user.email}})</a>      
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+  export default {
+    // async asyncData({store, error}) {
+    //   try {
+    //     await store.dispatch('users/fetchUsers')
+    //   return {}
+    //   } catch (e) {
+    //     error(e)
+    //   }
+    // },
+    // async fetch({store, error}) {
+    //   try {
+    //     if (store.getters['users/users'].length === 0) {
+    //       await store.dispatch('users/fetchUsers')
+    //     }
+    //   } catch (e) {
+    //     error(e)
+    //   }    
+    // },
+    data() {
+      return {
+        pageTitle: 'Users page'
+      }
+    },
+    computed: {
+      users() {
+        return this.$store.getters['users/users']
+      }
+    },
+    methods: {
+      goTo(user) {
+        this.$router.push('/users/' + user.id)
+      }
+    }
+  }
+</script>
